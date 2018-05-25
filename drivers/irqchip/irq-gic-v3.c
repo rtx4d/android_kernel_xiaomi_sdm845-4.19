344353b695534719ef2102dfed754cd75f19d0c0
Angelo G. Del Regno <kholk11@gmail.com>
2018-05-25 15:16:33 +0200

    irqchip: gic-v3: Add GIC-V3 redistributor hack
    Access to GIC Redistributor has to never be disabled or
    re-enabled because of ACLs set from secure world.
    This hack may be needed only on some QCOM SoCs.
    
    Add a new configuration option to enable this hack:
    CONFIG_ARM_GIC_V3_REDISTRIBUTOR_HACK

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -232,6 +232,9 @@ static u64 __maybe_unused gic_read_iar(void)
 }
 #endif
 
+#ifdef CONFIG_ARM_GIC_V3_REDISTRIBUTOR_HACK
+#define gic_enable_redist(x)
+#else
 static void gic_enable_redist(bool enable)
 {
 	void __iomem *rbase;
@@ -265,6 +268,7 @@ static void gic_enable_redist(bool enable)
 		pr_err_ratelimited("redistributor failed to %s...\n",
 				   enable ? "wakeup" : "sleep");
 }
+#endif
 
 
 void gic_v3_dist_save(void)
