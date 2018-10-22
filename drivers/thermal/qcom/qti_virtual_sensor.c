b6160c1fad95e05d2e0288e285ac989bc96972f4
Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>
2018-10-22 18:36:12 +0530

    drivers: thermal: virtual-sensor: Add new virtual sensor for MSM8917
    Add new virtual sensor definition for penta-cpu-max-step for MSM8917.
    This virtual sensor can be used to monitor the overall temperature
    of CPU cluster device.
    
    Change-Id: I6a587b42cb4a474e1ce53ebbf893bbc168f22be4
    Signed-off-by: Manaf Meethalavalappu Pallikunhi <manafm@codeaurora.org>

--- drivers/thermal/qcom/qti_virtual_sensor.c
+++ drivers/thermal/qcom/qti_virtual_sensor.c
@@ -175,6 +175,16 @@ static const struct virtual_sensor_data qti_virtual_sensors[] = {
 		.logic = VIRT_MAXIMUM,
 	},
 	{
+		.virt_zone_name = "penta-cpu-max-step",
+		.num_sensors = 5,
+		.sensor_names = {"apc1-cpu0-usr",
+				"apc1-cpu1-usr",
+				"apc1-cpu2-usr",
+				"apc1-cpu3-usr",
+				"cpuss-usr"},
+		.logic = VIRT_MAXIMUM,
+	},
+	{
 		.virt_zone_name = "gpu-skin-avg-step",
 		.num_sensors = 2,
 		.sensor_names = {"skin-msm-therm-usr",
