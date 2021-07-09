7da1c595b364eb7f1198b49bc236ee799d409c41
MrArtemSid <artem.sid.key@gmail.com>
2021-07-09 13:02:49 +0400

    drivers: gpu: msm: delete opp usage

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -858,120 +858,6 @@ int kgsl_busmon_get_cur_freq(struct device *dev, unsigned long *freq)
 }
 
 /*
- * opp_notify - Callback function registered to receive OPP events.
- * @nb: The notifier block
- * @type: The event type. Two OPP events are expected in this function:
- *      - OPP_EVENT_ENABLE: an GPU OPP is enabled. The in_opp parameter
- *	contains the OPP that is enabled
- *	- OPP_EVENT_DISALBE: an GPU OPP is disabled. The in_opp parameter
- *	contains the OPP that is disabled.
- * @in_opp: the GPU OPP whose status is changed and triggered the event
- *
- * GPU OPP event callback function. The function subscribe GPU OPP status
- * change and update thermal power level accordingly.
- */
-
-static int opp_notify(struct notifier_block *nb,
-	unsigned long type, void *in_opp)
-{
-	int result = -EINVAL, level, min_level, max_level;
-	struct kgsl_pwrctrl *pwr = container_of(nb, struct kgsl_pwrctrl, nb);
-	struct kgsl_device *device = container_of(pwr,
-			struct kgsl_device, pwrctrl);
-	struct device *dev = &device->pdev->dev;
-	struct dev_pm_opp *opp;
-	unsigned long min_freq = 0, max_freq = pwr->pwrlevels[0].gpu_freq;
-
-	if (type != OPP_EVENT_ENABLE && type != OPP_EVENT_DISABLE)
-		return result;
-
-	rcu_read_lock();
-	opp = dev_pm_opp_find_freq_floor(dev, &max_freq);
-	if (IS_ERR(opp)) {
-		rcu_read_unlock();
-		return PTR_ERR(opp);
-	}
-
-	opp = dev_pm_opp_find_freq_ceil(dev, &min_freq);
-	if (IS_ERR(opp))
-		min_freq = pwr->pwrlevels[pwr->min_pwrlevel].gpu_freq;
-
-	rcu_read_unlock();
-
-	mutex_lock(&device->mutex);
-
-	max_level = pwr->thermal_pwrlevel;
-	min_level = pwr->thermal_pwrlevel_floor;
-
-	/* Thermal limit cannot be lower than lowest non-zero operating freq */
-	for (level = 0; level < (pwr->num_pwrlevels - 1); level++) {
-		if (pwr->pwrlevels[level].gpu_freq == max_freq)
-			max_level = level;
-		if (pwr->pwrlevels[level].gpu_freq == min_freq)
-			min_level = level;
-	}
-
-	pwr->thermal_pwrlevel = max_level;
-	pwr->thermal_pwrlevel_floor = min_level;
-
-	/* Update the current level using the new limit */
-	kgsl_pwrctrl_pwrlevel_change(device, pwr->active_pwrlevel);
-	mutex_unlock(&device->mutex);
-
-	return 0;
-}
-
-/*
- * kgsl_opp_add_notifier - add a fine grained notifier.
- * @dev: The device
- * @nb: Notifier block that will receive updates.
- *
- * Add a notifier to receive GPU OPP_EVENT_* events
- * from the OPP framework.
- */
-static int kgsl_opp_add_notifier(struct device *dev,
-		struct notifier_block *nb)
-{
-	struct srcu_notifier_head *nh;
-	int ret = 0;
-
-	rcu_read_lock();
-	nh = dev_pm_opp_get_notifier(dev);
-	if (IS_ERR(nh))
-		ret = PTR_ERR(nh);
-	rcu_read_unlock();
-	if (!ret)
-		ret = srcu_notifier_chain_register(nh, nb);
-
-	return ret;
-}
-
-/*
- * kgsl_opp_remove_notifier - remove registered opp event notifier.
- * @dev: The device
- * @nb: Notifier block that will receive updates.
- *
- * Remove gpu notifier that receives GPU OPP_EVENT_* events
- * from the OPP framework.
- */
-static int kgsl_opp_remove_notifier(struct device *dev,
-		struct notifier_block *nb)
-{
-	struct srcu_notifier_head *nh;
-	int ret = 0;
-
-	rcu_read_lock();
-	nh = dev_pm_opp_get_notifier(dev);
-	if (IS_ERR(nh))
-		ret = PTR_ERR(nh);
-	rcu_read_unlock();
-	if (!ret)
-		ret = srcu_notifier_chain_unregister(nh, nb);
-
-	return ret;
-}
-
-/*
  * kgsl_pwrscale_init - Initialize pwrscale.
  * @dev: The device
  * @governor: The initial governor to use.
@@ -999,8 +885,6 @@ int kgsl_pwrscale_init(struct device *dev, const char *governor)
 	pwr = &device->pwrctrl;
 	gpu_profile = &pwrscale->gpu_profile;
 	profile = &pwrscale->gpu_profile.profile;
-
-	pwr->nb.notifier_call = opp_notify;
 
 	kgsl_opp_add_notifier(dev, &pwr->nb);
 
